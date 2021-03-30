28f8822f992bb39671012a20814f2a710560fd8f
Robin Christoffersen <robin.christoffersen@gmail.com>
2021-03-29 17:13:36 -0700

    Read csv file into a dictionary

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,35 +1,20 @@
 # PyBank analyze financial records
-
 # import modules 
 import os
 import csv
-from datetime import datetime
 
-def calc_age_months(from_date, to_date):
-    from_date = datetime.strptime(from_date, "%m-%Y")
-    to_date = datetime.strptime(to_date, "%m-%Y")
+input_file = os.path.join("Resources", "budget_data.csv")
 
-    age_in_months = (to_date.tm_year - from_date.tm_year)*12 + (to_date.tm_mon - from_date.tm_mon)
+sum_months = 0
+sum_revenue = 0
 
-    if to_date.tm_mday < from_date.tm_mday:
-        return age_in_months -1
-    else:
-        return age_in_months
+with open(input_file) as csvfile:
+    input_reader = csv.DictReader(csvfile)
 
-csvpath = os.path.join("Resources", "budget_data.csv")
-
-with open(csvpath) as csvfile:
-    CSVreader = csv.reader(csvfile, delimiter = ',')
+    for row in input_reader:
+        sum_months = sum_months + 1
+        sum_revenue = sum_revenue + int(row["Profit/Losses"])
     
-    # Read the header row first
-    csv_header = next(CSVreader)
-    print(f"CSV Header: {csv_header}")
-    dates = []
-    PL = []
-
-    for row in CSVreader:
-        dates.append(row[0])
-        PL.append(row[1])
-    print(f"First {dates[0]}, Last {dates[-1]} ")
-    print(f"First {PL[0]}, Last {PL[-1]} ")
-        
+    print(sum_months)
+    
+    
