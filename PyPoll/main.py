07aa051129f585bfd1f0b85430f0410a078afa3d
Robin Christoffersen <robin.christoffersen@gmail.com>
2021-04-01 17:05:03 -0700

    Updated PyPoll with percentage and vote count per candidate

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,36 +3,34 @@
 # import modules 
 import os
 import csv
-import collections
 
 input_file = os.path.join("Resources", "election_data.csv")
 
 num_votes = 0
-candidatelist = collections.defaultdict(list)
+candidatelist = []
+candidatevotes = {}
 
 with open(input_file) as csvfile:
+    # Headers: Voter ID,County,Candidate
     input_reader = csv.DictReader(csvfile)
 
     for row in input_reader:
         # calculate the total number of votes
         num_votes += 1
-
-        if row["Candidate"] not in candidatelist["Name"]:
-            candidatelist["Name"].append(row["Candidate"]) 
-            candidatelist[row["Candidate"]] = 1 
+        # https://www.geeksforgeeks.org/python-count-occurrences-of-each-word-in-given-text-file-using-dictionary/
+        if row["Candidate"] not in candidatelist:
+            candidatelist.append(row["Candidate"])
+            candidatevotes[row["Candidate"]] = 1
         else:
-            candidatelist[row["Candidate"]] += 1 
-
-    print("\n")
-    print("Election Results")
-    print("--------------------")
-    print(f"Total Votes: {num_votes}")
-    print("--------------------")
-    # To print all keys and values separated by commas
-    print('\n'.join(str(key) + ': ' + str(value) for key, value in candidatelist.items()))
-    print("--------------------")
-    print(f"Winner: {str(candidatelist[1])}")
-    print("--------------------")
+            candidatevotes[row["Candidate"]] = candidatevotes[row["Candidate"]] + 1
+        
+print("\n")
+print("Election Results")
+print("--------------------")
+print(f"Total Votes: {num_votes}")
+print("--------------------")
+for candidate in candidatevotes:
+    print(f"{candidate}: {(candidatevotes[candidate]/num_votes) * 100 :.3f}% {candidatevotes[candidate]}")
 
 #output_file = os.path.join("Analysis", "analysis.txt")
 #with open(output_file, 'w') as txtfile:
